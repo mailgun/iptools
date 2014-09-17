fac48f4f8a866e6fd74ecd4cc74f01fe36e6ef9c
Roman Tkachenko <roman@mailgunhq.com>
2014-09-17 11:18:47 -0700

    Fixes.

--- ip.go
+++ ip.go
@@ -3,7 +3,7 @@ package iptools
 import "net"
 
 // Ranges of addresses allocated by IANA for private internets, as per RFC1918.
-const PrivateNetworks = []string{
+var PrivateNetworks = []string{
 	"10.0.0.0/8",
 	"172.16.0.0/12",
 	"192.168.0.0/16",
@@ -28,7 +28,7 @@ func GetHostIPs() ([]net.IP, error) {
 
 // GetPrivateHostIPs returns a list of host's private IP addresses.
 func GetPrivateHostIPs() ([]net.IP, error) {
-	ips, err := GetIPs()
+	ips, err := GetHostIPs()
 	if err != nil {
 		return nil, err
 	}
@@ -46,7 +46,7 @@ func GetPrivateHostIPs() ([]net.IP, error) {
 }
 
 // IsPrivate determines whether a passed IP address is from one of private blocks or not.
-func IsPrivate(ip net.IP) (bool, error) {
+func IsPrivate(ip net.IP) bool {
 	for _, network := range PrivateNetworks {
 		_, ipnet, _ := net.ParseCIDR(network)
 		if ipnet.Contains(ip) {
